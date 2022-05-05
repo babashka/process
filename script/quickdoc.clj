e7579ea76a77f5d70ac3366fa6afec8b106faaea
Michiel Borkent <michielborkent@gmail.com>
2022-05-05 15:54:57 +0200

    quickdoc [skip ci]

---
+++ script/quickdoc.clj
@@ -0,0 +1,23 @@
+(ns quickdoc
+  (:require [pod.borkdude.clj-kondo :as clj-kondo]
+            [clojure.string :as str]))
+
+(def var-defs
+  (-> (clj-kondo/run! {:lint ["src"]
+                       :config {:output {:analysis true}}})
+      :analysis :var-definitions))
+
+(def nss (group-by :ns var-defs))
+
+(def docs
+  (with-out-str
+    (doseq [[ns ana] nss
+            :let [_ (println "##" ns)]
+            var ana]
+      (println "###" (:name var))
+      (when-let [doc (:doc var)]
+        (println doc)))))
+
+(spit "README.md"
+      (str/replace (slurp "README.md.template")
+                   "{{ quickdoc }}" docs))
