de0b743b25cbb8341bf19c0e575b7d77497a95ea
Michiel Borkent <michielborkent@gmail.com>
2023-01-26 16:45:27 +0100

    project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject babashka/process "0.4.15"
+(defproject babashka/process "0.4.16"
   :description "Clojure library for shelling out / spawning subprocesses"
   :url "https://github.com/babashka/process"
   :scm {:name "git"
